3eef3468df194260ffd889c3faf3a4c0397150eb
UMU18 <littlegodwizard@gmail.com>
2018-09-21 19:52:25 +0700

    hello.py

--- hello.py
+++ hello.py
@@ -111,5 +111,5 @@ def index():
 	
 
 if __name__ == '__main__':
-port = int(os.environ.get('PORT', 5000)) #The port to be listening to — hence, the URL must be <hostname>:<port>/ inorder to send the request to this program
-app.run(host='0.0.0.0', port=port)  #Start listening
+		port = int(os.environ.get('PORT', 5000)) #The port to be listening to — hence, the URL must be <hostname>:<port>/ inorder to send the request to this program
+		app.run(host='0.0.0.0', port=port)  #Start listening
