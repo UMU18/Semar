b3c80672984b377ddd888624c1a659ba237e4493
UMU18 <littlegodwizard@gmail.com>
2018-09-21 21:33:23 +0700

    hello.py

--- hello.py
+++ hello.py
@@ -90,10 +90,10 @@ def index():
 		if len(message) > 0:
 			vectorize_message = Vectorizer.transform([message])
 			predict = Classifier.predict(vectorize_message)[0]
-			predict_proba = Classifier.predict_proba(vectorize_message)
+			predict_proba = Classifier.predict_proba(vectorize_message).tolist()
 	except BaseException as inst:
 		error = str(type(inst).__name__) + ' ' + str(inst)
-	return jsonify( predict_proba )
+	return jsonify( predictproba=predict_proba )
 
 if __name__ == '__main__':
 		port = int(os.environ.get('PORT', 5000)) #The port to be listening to — hence, the URL must be <hostname>:<port>/ inorder to send the request to this program
