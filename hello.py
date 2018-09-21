f1a186f946f5b2a3b6e9f90483e1e53a57ca0e46
UMU18 <littlegodwizard@gmail.com>
2018-09-21 11:48:32 +0700

    hello.py

--- hello.py
+++ hello.py
@@ -8,13 +8,16 @@ DATABASE_URL = os.environ['DATABASE_URL']
 
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
-@app.route('/hello') #whenever this webserver is called with <hostname:port>/hello then this section is called
-def hello():
+def view:
 	show = "SELECT * FROM data_latih"
 	cur = conn.cursor()
 	cur.execute(show)
 	row = cur.fetchone()
-	print(cur.rowcount)
+	return row
+
+@app.route('/hello') #whenever this webserver is called with <hostname:port>/hello then this section is called
+def hello():
+	return view()
 	
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 5000)) #The port to be listening to — hence, the URL must be <hostname>:<port>/ inorder to send the request to this program
