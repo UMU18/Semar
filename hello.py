b9f97628c00c5cb318ab9c92f0cbef895b0d8b0b
UMU18 <littlegodwizard@gmail.com>
2018-09-21 14:13:01 +0700

    hello.py

--- hello.py
+++ hello.py
@@ -4,21 +4,18 @@ import psycopg2
 
 app = Flask(__name__) #create an instance of the Flask library
 
-DATABASE_URL = os.environ['data.heroku.com/datastores/7f5c6592-8985-46d0-a0c0-6791409c61f0']
+DATABASE_URL = os.environ['DATABASE_URL']
 
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 @app.route('/hello') #whenever this webserver is called with <hostname:port>/hello then this section is called
 def hello():
-	if conn!=Null:
-		cur = conn.cursor()
-		cur.execute("SELECT * FROM data_latih;")
-		raw = cur.fetchone()
-		return raw
-	else :
-		return 'llallalalala'
-
+	cur = conn.cursor()
+	cur.execute("SELECT * FROM data_latih;")
+	raw = cur.fetchone()
+	return raw['term']
 	
+
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 5000)) #The port to be listening to — hence, the URL must be <hostname>:<port>/ inorder to send the request to this program
 	app.run(host='0.0.0.0', port=port)  #Start listening
