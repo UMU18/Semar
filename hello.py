7baed9e93df11fb4559dab6e0e27d066a9112353
UMU18 <littlegodwizard@gmail.com>
2018-09-27 23:35:58 +0700

    all files

--- hello.py
+++ hello.py
@@ -147,7 +147,7 @@ def Create_Data():
 
 if __name__ == '__main__':
 	scheduler = BackgroundScheduler()
-    scheduler.add_job(func=learning, trigger="cron", hour=3, minute=5)
-    scheduler.start()
+	scheduler.add_job(func=learning, trigger="cron", hour=3, minute=5)
+	scheduler.start()
 	port = int(os.environ.get('PORT', 5000))
 	app.run(debug=True, use_reloader=False, host='0.0.0.0', port=port)
