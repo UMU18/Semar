bfbc8be56a0fce0438f7aaa66c6fe84126eb20f2
UMU123 <littlegodwizard@gmail.com>
2018-11-18 12:43:09 +0700

    update

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, request, jsonify
 import psycopg2
 from sqlalchemy import create_engine
 import pickle
@@ -8,56 +8,126 @@ from bs4 import BeautifulSoup
 import re
 import string
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+from collections import Counter
 
 
 app = Flask(__name__) #create an instance of the Flask library
 
+#connect database
 DATABASE_URL = os.environ['DATABASE_URL']
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 engine = create_engine(os.environ['DATABASE_URL'])
+
+#read dictionary
+def words(text): return re.findall(r'\w+', text.lower())
+
+WORDS = Counter(words(open('spellcheck.txt').read()))
+ROOTWORDS = Counter(words(open('kata-dasar.txt').read()))
+STOPWORDS = Counter(words(open('stopword.txt').read()))
 
 #remove URI
 uri_re = r'(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’]))'
 
 def stripTagsAndUris(x):
-    if x:
-        # BeautifulSoup on content
-        soup = BeautifulSoup(x, "html.parser")
-        # Stripping all <code> tags with their content if any
-        if soup.code:
-            soup.code.decompose()
-        # Get all the text out of the html
-        text =  soup.get_text()
-        # Returning text stripping out all uris
-        return re.sub(uri_re, "", text)
-    else:
-        return ""
-    
+	if x:
+		# BeautifulSoup on content
+		soup = BeautifulSoup(x, "html.parser")
+		# Stripping all <code> tags with their content if any
+		if soup.code:
+			soup.code.decompose()
+		# Get all the text out of the html
+		text =  soup.get_text()
+		# Returning text stripping out all uris
+		return re.sub(uri_re, "", text)
+	else:
+		return ""
+	
 #remove punctuation   
 def removePunctuation(x):
-    # Lowercasing all words
-    x = x.lower()
-    # Removing non ASCII chars
-    x = re.sub(r'[^\x00-\x7f]|(@[A-Za-z0-9]+)|(#[A-Za-z0-9]+)|([^a-zA-Z ]+?)',r' ',x)
-    # Removing (replacing with empty spaces actually) all the punctuations
-    return re.sub("["+string.punctuation+"]", " ", x)
+	# Lowercasing all words
+	x = x.lower()
+	# Removing non ASCII chars
+	x = re.sub(r'[^\x00-\x7f]|(@[a-z0-9_.-]+)|(#[A-Za-z0-9]+)|([^a-zA-Z ]+?)',r' ',x)
+	# Removing (replacing with empty spaces actually) all the punctuations
+	return re.sub("["+string.punctuation+"]", " ", x)
 
 #stemming
 factory = StemmerFactory()
 stemmer = factory.create_stemmer()
 
 def stemming(x):
-    return stemmer.stem(x)
+	return stemmer.stem(x)
+
+#handle lengthning word
+def shortening(x):
+	result = []
+	for word in x.split():
+		if word not in ROOTWORDS:
+			karakter = list(word)
+			for i in range(len(word)-1):
+				if karakter[i]==karakter[i+1]:
+					karakter[i]=''
+			word=''.join(karakter)
+			if word not in ROOTWORDS:
+				karakter = list(word)
+				for i in range(len(word)-3):
+					if karakter[i]==karakter[i+2] and karakter[i+1]==karakter[i+3]:
+						karakter[i]=''
+						karakter[i+1]=''
+				word=''.join(karakter)
+				if len(word)<4 and word not in ROOTWORDS:
+					word=''
+		result.append(word)
+		
+	return " ".join(result)
+
+def P(word, N=sum(WORDS.values())): 
+	# "Probability of `word`."
+	return WORDS[word] / N
+
+def correction(x):
+	word=shortening(x).split()
+	result=[]
+	for w in word:
+		if w not in ROOTWORDS:
+			w=max(candidates(w), key=P)
+		result.append(w)
+	return " ".join(result)
+
+def candidates(word): 
+	# "Generate possible spelling corrections for word."
+	if len (word) < 5:
+		candidate=known([word]) or known(edits1(word)) or [word]
+	else:
+		candidate=known([word]) or known(edits1(word)) or known(edits2(word)) or [word]
+	return candidate
+
+def known(words): 
+	# "The subset of `words` that appear in the dictionary of WORDS."
+	return set(w for w in words if w in WORDS)
+
+def edits1(word):
+	# "All edits that are one edit away from `word`."
+	letters    = 'abcdefghijklmnopqrstuvwxyz'
+	splits     = [(word[:i], word[i:])    for i in range(len(word) + 1)] # [('', 'kemarin'), ('k', 'emarin'), ('ke', 'marin'), dst]
+	deletes    = [L + R[1:]               for L, R in splits if R] # ['emarin', 'kmarin', 'kearin', dst]
+	transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R)>1] # ['ekmarin', 'kmearin', 'keamrin', dst]
+	replaces   = [L + c + R[1:]           for L, R in splits if R for c in letters] # ['aemarin', 'bemarin', 'cemarin', dst]
+	inserts    = [L + c + R               for L, R in splits for c in letters] # ['akemarin', 'bkemarin', 'ckemarin', dst]
+	return set(deletes + transposes + replaces + inserts)
+
+def edits2(word): 
+	# "All edits that are two edits away from `word`."
+	return (e2 for e1 in edits1(word) for e2 in edits1(e1))
 
 #remove stopword
-stopword = {'ada', 'adalah', 'adanya', 'adapun', 'agak', 'agaknya', 'agar', 'akan', 'akankah', 'akhir', 'akhiri', 'akhirnya', 'aku', 'akulah', 'amat', 'amatlah', 'anda', 'andalah', 'antar', 'antara', 'antaranya', 'apa', 'apaan', 'apabila', 'apakah', 'apalagi', 'apatah', 'artinya', 'asal', 'asalkan', 'atas', 'atau', 'ataukah', 'ataupun', 'awal', 'awalnya', 'bagai', 'bagaikan', 'bagaimana', 'bagaimanakah', 'bagaimanapun', 'bagi', 'bagian', 'bahkan', 'bahwa', 'bahwasanya', 'baik', 'bakal', 'bakalan', 'balik', 'banyak', 'bapak', 'baru', 'bawah', 'beberapa', 'begini', 'beginian', 'beginikah', 'beginilah', 'begitu', 'begitukah', 'begitulah', 'begitupun', 'bekerja', 'belakang', 'belakangan', 'belum', 'belumlah', 'benar', 'benarkah', 'benarlah', 'berada', 'berakhir', 'berakhirlah', 'berakhirnya', 'berapa', 'berapakah', 'berapalah', 'berapapun', 'berarti', 'berawal', 'berbagai', 'berdatangan', 'beri', 'berikan', 'berikut', 'berikutnya', 'berjumlah', 'berkali-kali', 'berkata', 'berkehendak', 'berkeinginan', 'berkenaan', 'berlainan', 'berlalu', 'berlangsung', 'berlebihan', 'bermacam', 'bermacam-macam', 'bermaksud', 'bermula', 'bersama', 'bersama-sama', 'bersiap', 'bersiap-siap', 'bertanya', 'bertanya-tanya', 'berturut', 'berturut-turut', 'bertutur', 'berujar', 'berupa', 'besar', 'betul', 'betulkah', 'biasa', 'biasanya', 'bila', 'bilakah', 'bisa', 'bisakah', 'boleh', 'bolehkah', 'bolehlah', 'bu', 'buat', 'bukan', 'bukankah', 'bukanlah', 'bukannya', 'bulan', 'bung', 'cara', 'caranya', 'cukup', 'cukupkah', 'cukuplah', 'cuma', 'dahulu', 'dalam', 'dan', 'dapat', 'dari', 'daripada', 'datang', 'dekat', 'demi', 'demikian', 'demikianlah', 'dengan', 'depan', 'di', 'dia', 'diakhiri', 'diakhirinya', 'dialah', 'diantara', 'diantaranya', 'diberi', 'diberikan', 'diberikannya', 'dibuat', 'dibuatnya', 'didapat', 'didatangkan', 'digunakan', 'diibaratkan', 'diibaratkannya', 'diingat', 'diingatkan', 'diinginkan', 'dijawab', 'dijelaskan', 'dijelaskannya', 'dikarenakan', 'dikatakan', 'dikatakannya', 'dikerjakan', 'diketahui', 'diketahuinya', 'dikira', 'dilakukan', 'dilalui', 'dilihat', 'dimaksud', 'dimaksudkan', 'dimaksudkannya', 'dimaksudnya', 'diminta', 'dimintai', 'dimisalkan', 'dimulai', 'dimulailah', 'dimulainya', 'dimungkinkan', 'dini', 'dipastikan', 'diperbuat', 'diperbuatnya', 'dipergunakan', 'diperkirakan', 'diperlihatkan', 'diperlukan', 'diperlukannya', 'dipersoalkan', 'dipertanyakan', 'dipunyai', 'diri', 'dirinya', 'disampaikan', 'disebut', 'disebutkan', 'disebutkannya', 'disini', 'disinilah', 'ditambahkan', 'ditandaskan', 'ditanya', 'ditanyai', 'ditanyakan', 'ditegaskan', 'ditujukan', 'ditunjuk', 'ditunjuki', 'ditunjukkan', 'ditunjukkannya', 'ditunjuknya', 'dituturkan', 'dituturkannya', 'diucapkan', 'diucapkannya', 'diungkapkan', 'dong', 'dua', 'dulu', 'empat', 'enggak', 'enggaknya', 'entah', 'entahlah', 'guna', 'gunakan', 'hal', 'hampir', 'hanya', 'hanyalah', 'hari', 'harus', 'haruslah', 'harusnya', 'hendak', 'hendaklah', 'hendaknya', 'hingga', 'ia', 'ialah', 'ibarat', 'ibaratkan', 'ibaratnya', 'ibu', 'ikut', 'ingat', 'ingat-ingat', 'ingin', 'inginkah', 'inginkan', 'ini', 'inikah', 'inilah', 'itu', 'itukah', 'itulah', 'jadi', 'jadilah', 'jadinya', 'jangan', 'jangankan', 'janganlah', 'jauh', 'jawab', 'jawaban', 'jawabnya', 'jelas', 'jelaskan', 'jelaslah', 'jelasnya', 'jika', 'jikalau', 'juga', 'jumlah', 'jumlahnya', 'justru', 'kala', 'kalau', 'kalaulah', 'kalaupun', 'kalian', 'kami', 'kamilah', 'kamu', 'kamulah', 'kan', 'kapan', 'kapankah', 'kapanpun', 'karena', 'karenanya', 'kasus', 'kata', 'katakan', 'katakanlah', 'katanya', 'ke', 'keadaan', 'kebetulan', 'kecil', 'kedua', 'keduanya', 'keinginan', 'kelamaan', 'kelihatan', 'kelihatannya', 'kelima', 'keluar', 'kembali', 'kemudian', 'kemungkinan', 'kemungkinannya', 'kenapa', 'kepada', 'kepadanya', 'kesampaian', 'keseluruhan', 'keseluruhannya', 'keterlaluan', 'ketika', 'khususnya', 'kini', 'kinilah', 'kira', 'kira-kira', 'kiranya', 'kita', 'kitalah', 'kok', 'kurang', 'lagi', 'lagian', 'lah', 'lain', 'lainnya', 'lalu', 'lama', 'lamanya', 'lanjut', 'lanjutnya', 'lebih', 'lewat', 'lima', 'luar', 'macam', 'maka', 'makanya', 'makin', 'malah', 'malahan', 'mampu', 'mampukah', 'mana', 'manakala', 'manalagi', 'masa', 'masalah', 'masalahnya', 'masih', 'masihkah', 'masing', 'masing-masing', 'mau', 'maupun', 'melainkan', 'melakukan', 'melalui', 'melihat', 'melihatnya', 'memang', 'memastikan', 'memberi', 'memberikan', 'membuat', 'memerlukan', 'memihak', 'meminta', 'memintakan', 'memisalkan', 'memperbuat', 'mempergunakan', 'memperkirakan', 'memperlihatkan', 'mempersiapkan', 'mempersoalkan', 'mempertanyakan', 'mempunyai', 'memulai', 'memungkinkan', 'menaiki', 'menambahkan', 'menandaskan', 'menanti', 'menanti-nanti', 'menantikan', 'menanya', 'menanyai', 'menanyakan', 'mendapat', 'mendapatkan', 'mendatang', 'mendatangi', 'mendatangkan', 'menegaskan', 'mengakhiri', 'mengapa', 'mengatakan', 'mengatakannya', 'mengenai', 'mengerjakan', 'mengetahui', 'menggunakan', 'menghendaki', 'mengibaratkan', 'mengibaratkannya', 'mengingat', 'mengingatkan', 'menginginkan', 'mengira', 'mengucapkan', 'mengucapkannya', 'mengungkapkan', 'menjadi', 'menjawab', 'menjelaskan', 'menuju', 'menunjuk', 'menunjuki', 'menunjukkan', 'menunjuknya', 'menurut', 'menuturkan', 'menyampaikan', 'menyangkut', 'menyatakan', 'menyebutkan', 'menyeluruh', 'menyiapkan', 'merasa', 'mereka', 'merekalah', 'merupakan', 'meski', 'meskipun', 'meyakini', 'meyakinkan', 'minta', 'mirip', 'misal', 'misalkan', 'misalnya', 'mula', 'mulai', 'mulailah', 'mulanya', 'mungkin', 'mungkinkah', 'nah', 'naik', 'namun', 'nanti', 'nantinya', 'nyaris', 'nyatanya', 'oleh', 'olehnya', 'pada', 'padahal', 'padanya', 'pak', 'paling', 'panjang', 'pantas', 'para', 'pasti', 'pastilah', 'penting', 'pentingnya', 'per', 'percuma', 'perlu', 'perlukah', 'perlunya', 'pernah', 'persoalan', 'pertama', 'pertama-tama', 'pertanyaan', 'pertanyakan', 'pihak', 'pihaknya', 'pukul', 'pula', 'pun', 'punya', 'rasa', 'rasanya', 'rata', 'rupanya', 'saat', 'saatnya', 'saja', 'sajalah', 'saling', 'sama', 'sama-sama', 'sambil', 'sampai', 'sampai-sampai', 'sampaikan', 'sana', 'sangat', 'sangatlah', 'satu', 'saya', 'sayalah', 'se', 'sebab', 'sebabnya', 'sebagai', 'sebagaimana', 'sebagainya', 'sebagian', 'sebaik', 'sebaik-baiknya', 'sebaiknya', 'sebaliknya', 'sebanyak', 'sebegini', 'sebegitu', 'sebelum', 'sebelumnya', 'sebenarnya', 'seberapa', 'sebesar', 'sebetulnya', 'sebisanya', 'sebuah', 'sebut', 'sebutlah', 'sebutnya', 'secara', 'secukupnya', 'sedang', 'sedangkan', 'sedemikian', 'sedikit', 'sedikitnya', 'seenaknya', 'segala', 'segalanya', 'segera', 'seharusnya', 'sehingga', 'seingat', 'sejak', 'sejauh', 'sejenak', 'sejumlah', 'sekadar', 'sekadarnya', 'sekali', 'sekali-kali', 'sekalian', 'sekaligus', 'sekalipun', 'sekarang', 'sekecil', 'seketika', 'sekiranya', 'sekitar', 'sekitarnya', 'sekurang-kurangnya', 'sekurangnya', 'sela', 'selagi', 'selain', 'selaku', 'selalu', 'selama', 'selama-lamanya', 'selamanya', 'selanjutnya', 'seluruh', 'seluruhnya', 'semacam', 'semakin', 'semampu', 'semampunya', 'semasa', 'semasih', 'semata', 'semata-mata', 'semaunya', 'sementara', 'semisal', 'semisalnya', 'sempat', 'semua', 'semuanya', 'semula', 'sendiri', 'sendirian', 'sendirinya', 'seolah', 'seolah-olah', 'seorang', 'sepanjang', 'sepantasnya', 'sepantasnyalah', 'seperlunya', 'seperti', 'sepertinya', 'sepihak', 'sering', 'seringnya', 'serta', 'serupa', 'sesaat', 'sesama', 'sesampai', 'sesegera', 'sesekali', 'seseorang', 'sesuatu', 'sesuatunya', 'sesudah', 'sesudahnya', 'setelah', 'setempat', 'setengah', 'seterusnya', 'setiap', 'setiba', 'setibanya', 'setidak-tidaknya', 'setidaknya', 'setinggi', 'seusai', 'sewaktu', 'siap', 'siapa', 'siapakah', 'siapapun', 'sini', 'sinilah', 'soal', 'soalnya', 'suatu', 'sudah', 'sudahkah', 'sudahlah', 'supaya', 'tadi', 'tadinya', 'tahu', 'tahun', 'tak', 'tambah', 'tambahnya', 'tampak', 'tampaknya', 'tandas', 'tandasnya', 'tanpa', 'tanya', 'tanyakan', 'tanyanya', 'tapi', 'tegas', 'tegasnya', 'telah', 'tempat', 'tengah', 'tentang', 'tentu', 'tentulah', 'tentunya', 'tepat', 'terakhir', 'terasa', 'terbanyak', 'terdahulu', 'terdapat', 'terdiri', 'terhadap', 'terhadapnya', 'teringat', 'teringat-ingat', 'terjadi', 'terjadilah', 'terjadinya', 'terkira', 'terlalu', 'terlebih', 'terlihat', 'termasuk', 'ternyata', 'tersampaikan', 'tersebut', 'tersebutlah', 'tertentu', 'tertuju', 'terus', 'terutama', 'tetap', 'tetapi', 'tiap', 'tiba', 'tiba-tiba', 'tidak', 'tidakkah', 'tidaklah', 'tiga', 'tinggi', 'toh', 'tunjuk', 'turut', 'tutur', 'tuturnya', 'ucap', 'ucapnya', 'ujar', 'ujarnya', 'umum', 'umumnya', 'ungkap', 'ungkapnya', 'untuk', 'usah', 'usai', 'waduh', 'wah', 'wahai', 'waktu', 'waktunya', 'walau', 'walaupun', 'wong', 'yaitu', 'yakin', 'yakni', 'yang'}
-
 def removeStopwords(x):
-    # Removing all the stopwords
-    filtered_words = [word for word in x.split() if word not in stopword]
-    return " ".join(filtered_words)
+	# Removing all the stopwords
+	filtered_words = [word for word in x.split() if word not in STOPWORDS]
+	
+	return " ".join(filtered_words)
 
-@app.route('/', methods=['GET'])
+@app.route('/predict', methods=['GET'])
 def index():
 	arr_message = request.args.getlist('message')
 	error = ''
@@ -72,6 +142,7 @@ def index():
 				b=removePunctuation(a)
 				c=removeStopwords(b)
 				d=stemming(c)
+				e=correction(d)
 
 				select_vectorizer="SELECT vectorizer FROM model"
 				select_classifier="SELECT classifier FROM model"
@@ -88,7 +159,7 @@ def index():
 					unpickling_classifier.append(unclassy)
 				loadvectorizer=pickle.loads(b"".join(unpickling_vectorizer))
 				loadclassifier=pickle.loads(b"".join(unpickling_classifier))
-				vectorize_message = loadvectorizer.transform([d])
+				vectorize_message = loadvectorizer.transform([e])
 				predict = loadclassifier.predict(vectorize_message)[0]
 				predict_proba = loadclassifier.predict_proba(vectorize_message).tolist()
 				dicti = {}
@@ -101,30 +172,37 @@ def index():
 			error = str(type(inst).__name__) + ' ' + str(inst)
 	return jsonify(return_arr)
 
-@app.route('/', methods=['POST'])
+@app.route('/feedback', methods=['POST'])
 def Create_Data():
 	json = {
-	'predict': request.json['predict'],
+	'category': request.json['category'],
 	'message': request.json['message']}
+
+	a=stripTagsAndUris(json['message'])
+	b=removePunctuation(a)
+	c=removeStopwords(b)
+	d=stemming(c)
+	e=correction(d)
 
 	data = pd.read_sql_table("data_latih", engine, columns=['label', 'term'])
 	for index, row in data.iterrows():
-		if row["term"] == json['message'] and row["label"] != json['predict']:
+		if row["term"] == e and row["label"] != json['category']:
 			status = 'inconsisten'
 			break
-		elif row["term"] == json['message'] and row["label"] == json['predict']:
+		elif row["term"] == e and row["label"] == json['category']:
 			status = 'duplicate'
 			break
 		else:
 			status = 'not exist'
+
 	print(status)
 	if status == 'inconsisten':
 		delete_str = "DELETE FROM data_latih WHERE term = %s"
 		cur = conn.cursor()
-		cur.execute(delete_str, (json['message'],))
+		cur.execute(delete_str, (e,))
 		conn.commit()
 	if status == 'not exist':
-		dataInsert = pd.DataFrame({'label':[json['predict']],'term':[json['message']]})
+		dataInsert = pd.DataFrame({'label':[json['category']],'term':[d]})
 		dataInsert.to_sql('data_latih', engine, if_exists='append',index=False)
 	return jsonify(json), 201
 
